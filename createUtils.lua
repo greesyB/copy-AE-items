79b14797ac5beff23ee79698de6613433c32157c
greesyB <greesybuns@gmail.com>
2021-07-15 16:01:11 -0500

    rename createUtils

---
+++ createUtils.lua
@@ -0,0 +1,60 @@
+local debug = require("component").debug
+
+--local x = -4333
+--local y = 76
+--local z = 262
+
+--[[
+  Must pass:
+  id (String <mod:item>)
+  damage (Num <damage>)
+  nbt (String <stringified json>)
+  amount (Num <amount>)
+--]]
+
+-- Assumes overworld
+local world = debug.getWorld(0)
+
+local create = {}
+
+-- Inserts at most one stack of the given item
+function create.insert(id, amount, damage, nbt, x, y, z, side)
+  side = side or 1
+
+  --print("Attempting to insert item ", id, damage, "to location", x, y, z, "side:", side)
+  
+  local inserted = world.insertItem(id, amount, damage, nbt, x, y, z, side)
+
+  return inserted
+end
+
+-- Loops to insert the `amount` of a given item in its entirety
+function create.insertAll(id, amount, damage, nbt, x, y, z, side)
+  local numInserted = 0
+  local timesInserted = 0
+
+  while not(numInserted == amount) do
+    local toInsert = 64
+
+    if amount - numInserted < 64 then
+      toInsert = amount - numInserted
+    end
+
+    -- 9 slots in interface
+    if timesInserted % 9 == 0 then
+      os.sleep(0.1)
+    end
+
+    local wasInserted = create.insert(id, toInsert, damage, nbt, x, y, z)
+
+    if not(wasInserted) then break end
+
+    numInserted = numInserted + toInsert
+    timesInserted = timesInserted + 1
+  end
+
+  print("Inserted:", numInserted, "of", id, damage)
+
+end
+
+return create
