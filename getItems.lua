272c4df2e8c26714f81b958a9ff90d4d1538147e
greesyB <greesybuns@gmail.com>
2021-07-15 13:51:53 -0500

    better nbt, test creation

--- getItems.lua
+++ getItems.lua
@@ -1,25 +1,53 @@
 local comp = require("component")
 local controller = comp.me_controller
 local itemList = controller.allItems()
+local create = require("createItems")
+
+local ignoreTags = ["transferLimit", "size", "maxSize"]
 
 for e in itemList do
-  local itemData = {}
+  local id = ""
+  local amount = 1
+  local damage = 0.0
   local nbt = "{"
+
   for k,v in pairs(e) do
-    local itemVal = ""
+    if (k not "transferLimit" and k not "size" and k not "maxSize") then
+      local itemVal = ""
 
-    if type(v) == "string" then
-      itemVal = "\\\"" .. v .. "\\\""
-    else
-      itemVal = tostring(v)
+      if type(v) == "string" then
+        itemVal = "\"" .. v .. "\""
+      else
+        itemVal = tostring(v)
+      end
+
+      itemVal = itemVal .. ","
+
+      local itemString = k .. ": " .. itemVal
+      nbt = nbt .. itemString
+
+      if k == "name" then
+        id = v
+      end
+
+      if k == "damage" then
+        damage = v
+      end
+
     end
-    itemVal = itemVal .. ","
 
-    local itemString = k .. ": " .. itemVal
-    nbt = nbt .. itemString
+    if k == "size" then
+      amount = v
+    end
+
   end
 
+  nbt = nbt.gsub(nbt, ", $", "")
   nbt = nbt .. "}"
   print(nbt)
+
+  local inserted = create.insert(id, amount, damage, nbt, x, y, z)
+  print("Inserted:", inserted)
+
   print("")
 end
